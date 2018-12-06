8c32e7b77c62e1b02dd2160f77c334efd2ea85ee
darryllp <44566158+darryllp@users.noreply.github.com>
2018-12-06 02:32:45 -0800

    Add files via upload

---
+++ test/includes/dbh.inc.php
@@ -0,0 +1,8 @@
+<?php
+
+$servername = "Matalistdb.cbhzd1fnevha.us-east-2.rds.amazonaws.com";
+$username = "matalist";
+$password = "csunmatalist";
+$dbname = "matalistdb";
+
+$conn = mysqli_connect($servername, $username, $password, $dbname);
